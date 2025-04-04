94e83b681a900a5a1737282a360602b9c78826eb
a3p <me@a3p.re>
2025-04-04 21:48:40 +0100

    remove app creds setting for google and ignore extra env vars

--- app/config.py
+++ app/config.py
@@ -2,13 +2,12 @@ from pydantic_settings import BaseSettings
 from pydantic import ConfigDict
 
 class Settings(BaseSettings):
-    model_config = ConfigDict(env_file=".env")
+    model_config = ConfigDict(env_file=".env", extra="ignore")
     
     google_client_id: str
     google_client_secret: str
     gemini_api_key: str
     secret_key: str
     algorithm: str = "HS256"
-    google_application_credentials: str
 
 settings = Settings()
